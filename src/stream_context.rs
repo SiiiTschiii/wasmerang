6797ae548138d19ff52c71e09dac0eacbca2b770
Christof Gerber <christof.gerber1@gmail.com>
2025-06-06 10:36:03 +0200

    Cleanup

--- src/stream_context.rs
+++ src/stream_context.rs
@@ -20,42 +20,42 @@ impl StreamContext for DestIpLogger {
      // See https://github.com/proxy-wasm/proxy-wasm-rust-sdk/blob/main/src/traits.rs#L259
     fn on_new_connection(&mut self) -> Action {
         info!("[TCP WASM] New connection established (context #{})", self.context_id);
-        // Log source address
+        // Log destination address
         // Property names based on the Proxy-Wasm ABI version 0.2.1
         // https://github.com/proxy-wasm/spec/tree/main/abi-versions/v0.2.1#downstream-connection-properties
-        if let Some(val) = self.get_property(vec!["source", "address"]) {
-            if let Ok(s) = String::from_utf8(val) {
-                info!("[TCP WASM] Source address: {}", s);
-            } else {
-                info!("[TCP WASM] Source address: (non-UTF8)");
-            }
-        } else {
-            info!("[TCP WASM] Source address not found");
-        }
-        // Log destination address and set reroute metadata
-        let mut reroute_cluster: Option<&str> = None;
         if let Some(val) = self.get_property(vec!["destination", "address"]) {
             if let Ok(s) = String::from_utf8(val) {
                 info!("[TCP WASM] Destination address: {}", s);
-                // Parse last octet
-                if let Some(ip_part) = s.split(':').next() {
-                    if let Some(last_octet) = ip_part.split('.').last() {
-                        if let Ok(num) = last_octet.parse::<u8>() {
-                            if num % 2 == 0 {
-                                // Even last octet, reroute to egress2
-                                reroute_cluster = Some("egress2");
-                            } else {
-                                // Odd last octet, reroute to egress1
-                                reroute_cluster = Some("egress1");
-                            }
-                        }
-                    }
-                }
             } else {
                 info!("[TCP WASM] Destination address: (non-UTF8)");
             }
         } else {
             info!("[TCP WASM] Destination address not found");
+        }
+        // Log soruce address and set reroute metadata
+        let mut reroute_cluster: Option<&str> = None;
+        if let Some(val) = self.get_property(vec!["source", "address"]) {
+            if let Ok(s) = String::from_utf8(val) {
+                info!("[TCP WASM] Source address: {}", s);
+                // Parse last octet
+                if let Some(ip_part) = s.split(':').next() {
+                    if let Some(last_octet) = ip_part.split('.').last() {
+                        if let Ok(num) = last_octet.parse::<u8>() {
+                            if num % 2 == 0 {
+                                // Even last octet, reroute to egress1
+                                reroute_cluster = Some("egress1");
+                            } else {
+                                // Odd last octet, reroute to egress2
+                                reroute_cluster = Some("egress2");
+                            }
+                        }
+                    }
+                }
+            } else {
+                info!("[TCP WASM] Source address: (non-UTF8)");
+            }
+        } else {
+            info!("[TCP WASM] Source address not found");
         }
         // Set dynamic metadata for rerouting if needed
         if let Some(cluster) = reroute_cluster {
